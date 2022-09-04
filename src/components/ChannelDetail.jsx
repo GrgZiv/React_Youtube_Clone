676ceb0bde78c4cc3dc6664593e228d0b210f6dd
GrgZiv <imeiprezimepo@gmail.com>
2022-09-04 11:08:36 +0200

    initial commit

---
+++ src/components/ChannelDetail.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+import { Videos, ChannelCard } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+
+const ChannelDetail = () => {
+  const { id } = useParams();
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) => {
+      setChannelDetail(data?.items[0]);
+    });
+
+    fetchFromAPI(`search?part=snippet&channelId=${id}`).then((data) => {
+      setVideos(data?.items.slice(4, 54));
+    });
+  }, [id]);
+  console.log(videos);
+  return (
+    <Box minHeight="95vh">
+      <Box>
+        <div
+          style={{
+            background:
+              "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 0%, rgba(0,212,255,1) 50%, rgba(0,212,255,1) 100%)",
+            zIndex: 10,
+            height: "300px",
+          }}
+        />
+        <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
+      </Box>
+      <Box display="flex" p="2">
+        <Box sx={{ mr: { sm: "100px" } }} />
+        <Videos videos={videos} />
+      </Box>
+    </Box>
+  );
+};
+
+export default ChannelDetail;
---
+++ src/components/Feed.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import { SideBar, Videos } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+
+const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const realVideos = [];
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) => {
+      for (const key in data.items) {
+        data.items[key].id.videoId && realVideos.push(data.items[key]);
+      }
+      setVideos(realVideos);
+    });
+  }, [selectedCategory]);
+
+  return (
+    <Stack
+      sx={{
+        flexDirection: {
+          sx: "column",
+          md: "row",
+        },
+      }}
+    >
+      <Box
+        sx={{
+          height: {
+            sx: "auto",
+            md: "96vh",
+          },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <SideBar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
+        <Typography
+          className="copyright"
+          sx={{ mt: 1.5, color: "#fff" }}
+          variant="body2"
+        >
+          Copyright 2022 GrgZiv
+        </Typography>
+      </Box>
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          {selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
+        </Typography>
+        <Videos videos={videos} />
+      </Box>
+    </Stack>
+  );
+};
+
+export default Feed;
